8b456aef36316003a39e25fb0b73946f6159f7a1
Benjamin Schwartz <bmschwar@gmail.com>
2012-04-06 04:35:30 -0700

    small code cleanup: remove TODOs and give arguments names

--- src/main/java/com/bsg/InvalidConfigException.java
+++ src/main/java/com/bsg/InvalidConfigException.java
@@ -5,22 +5,19 @@ public class InvalidConfigException extends Exception {
 	private static final long serialVersionUID = 4387249066079104852L;
 
 	public InvalidConfigException() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
-	public InvalidConfigException(String arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
+	public InvalidConfigException(String msg) {
+		super(msg);
 	}
 
-	public InvalidConfigException(Throwable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
+	public InvalidConfigException(Throwable cause) {
+		super(cause);
 	}
 
-	public InvalidConfigException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
+	public InvalidConfigException(String msg, Throwable cause) {
+		super(msg, cause);
 	}
 
 }
