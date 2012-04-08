f4f261af865c5a4b3eef701a000e28381e144087
Benjamin Schwartz <bmschwar@gmail.com>
2012-04-07 17:54:14 -0700

    LoyaltyLoader refactored

--- src/main/java/com/bsg/cards/loyalty/LoyaltyCardLoader.java
+++ src/main/java/com/bsg/cards/loyalty/LoyaltyCardLoader.java
@@ -5,62 +5,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import com.bsg.Expansion;
 import com.bsg.InvalidConfigException;
+import com.bsg.utils.Loader;
 
-public class LoyaltyCardLoader {
+public class LoyaltyCardLoader extends Loader {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoyaltyCardLoader.class);
 	
-	private File xmlFile;
 	private List<LoyaltyCard> cards;
-	private Document doc;
 	
 	public LoyaltyCardLoader(File xmlFile) throws ParserConfigurationException, SAXException, IOException, InvalidConfigException {
-		this.xmlFile = xmlFile;
-		
+		super(xmlFile);
 		cards = new ArrayList<LoyaltyCard>();
-		
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		doc = db.parse(xmlFile);
-		
+
 		parse();
 	}
 
-	private void parse() throws InvalidConfigException {
-		Element root = doc.getDocumentElement();
-		String expansion_str = root.getAttribute("expansion");
-		LOGGER.info("Read expansion {}", expansion_str);
-		Expansion exp = null;
-		try {
-			exp = Expansion.valueOf(expansion_str);
-		} catch (Exception e) {
-			throw new InvalidConfigException(String.format("%s: Expansion \"%s\" is not valid", xmlFile.getName(), expansion_str));
-			
-		}
-
-		NodeList nl = root.getElementsByTagName("loyaltycard");
-		if (nl == null)
-			return;
-		
-		LOGGER.info("Read {} loyalty cards from {}", nl.getLength(), xmlFile.getName());
-		
-		for (int i = 0; i < nl.getLength(); ++i) {
-			Element el = (Element) nl.item(i);
-			
+	protected void parseItem(Element el) throws InvalidConfigException {
 			String title = el.getAttribute("title");
 			LoyaltyCardType type = LoyaltyCardType.valueOf(el.getAttribute("type"));
 			if (type == null)
@@ -68,12 +36,14 @@ public class LoyaltyCardLoader {
 			String text = el.getTextContent();
 			
 			LOGGER.info("Found card {}", title);
-			cards.add(new LoyaltyCard(title, type, text, exp));
-			
-		}
+			cards.add(new LoyaltyCard(title, type, text, expansion));
 	}
 	
 	public List<LoyaltyCard> getCards() {
 		return new ArrayList<LoyaltyCard>(cards);
+	}
+	
+	protected String getTagName() {
+		return "loyaltycard";
 	}
 }
--- src/main/java/com/bsg/utils/Loader.java
+++ src/main/java/com/bsg/utils/Loader.java
@@ -1,5 +1,64 @@
 package com.bsg.utils;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.bsg.Expansion;
+import com.bsg.InvalidConfigException;
+
 public abstract class Loader {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Loader.class);
+	
+	protected File xmlFile;
+	protected Document doc;
+	protected Expansion expansion;
+	
+	
+	public Loader(File xmlFile) throws ParserConfigurationException, SAXException, IOException, InvalidConfigException {
+		this.xmlFile = xmlFile;
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		
+		doc = db.parse(xmlFile);
+		LOGGER.info("Loaded file {}", xmlFile.getAbsolutePath());
+	}
+	
+	protected void parse() throws InvalidConfigException {
+		Element root = doc.getDocumentElement();
+		String expansion_str = root.getAttribute("expansion");
+		try {
+			expansion = Expansion.valueOf(expansion_str);
+		} catch (Exception e) {
+			throw new InvalidConfigException(String.format("%s: Expansion \"%s\" is not valid", xmlFile.getName(), expansion_str));
+		}
+		
+		NodeList nl = root.getElementsByTagName(getTagName());
+		if (nl == null)
+			return;
+		
+		for (int i = 0; i < nl.getLength(); ++i) {
+			Element el = (Element) nl.item(i);
+			parseItem(el);
+		}
+		
+		
+	}
+	
+	protected abstract String getTagName();
+	protected abstract void parseItem(Element el) throws InvalidConfigException;
+	
+	
 }
